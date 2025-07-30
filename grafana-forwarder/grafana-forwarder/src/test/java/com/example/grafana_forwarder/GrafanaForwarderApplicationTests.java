3aeca25a9d9227746de94e6b7f7fd47c1f3cf863
ddeniro <dangelo.deniro@greymatersdefense>
2025-07-30 13:13:14 -0600

    Added SpringBoot

---
+++ grafana-forwarder/grafana-forwarder/src/test/java/com/example/grafana_forwarder/GrafanaForwarderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.grafana_forwarder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrafanaForwarderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
